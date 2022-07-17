9410883e0431ff62ec6e3022e0e67f0eb11720f0
Vadim <Shamray-95@bk.ru>
2022-07-17 14:17:48 +0700

    1.16 test added

--- 1.16.scm
+++ 1.16.scm
@@ -7,3 +7,10 @@
       (else (iter (* a bn) (- counter 1) bn))))
   (iter b n b))
 
+;; для сравнения скорости 
+(define (i-expt b n)
+  (define (iter product counter)
+    (if (= counter 1) 
+      product
+      (iter (* product b) (- counter 1))))
+  (iter b n))

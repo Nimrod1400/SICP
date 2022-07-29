d8f761a2506ea4981340e8a938a3727491a844e1
Vadim <Shamray-95@bk.ru>
2022-07-29 16:55:18 +0700

    Revert "sorted by sections"
    This reverts commit cf4bdcdab243bd9d7f00f7b213e7bf602f7f3eb9.

--- 1.1/1.3.scm
+++
@@ -1,9 +0,0 @@
-(define (square x) (* x x))
-(define (sum-of-squares a b) (+ (square a) (square b)))
-(define (f a b c)
-	(cond 
-		((and (> a b) (> b c)) (sum-of-squares a b))
-		((and (> a b) (> c b)) (sum-of-squares a c))
-		((and (> b a) (> c b)) (sum-of-squares b c))		
-		((and (> b a) (> a c) (> a c)) (sum-of-squares  a b))		
-		((and (> b a) (> a c) (> c a)) (sum-of-squares  c b))))
--- 1.2/1.12.scm
+++
@@ -1,6 +0,0 @@
-(define (pascal x y)
-  (if (or (= y 1) (= x 1) (= x y))
-    1
-    (+ 
-      (pascal (- y 1) (- x 1))
-      (pascal (- y 1) x))))

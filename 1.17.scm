08e88f80c56cd3e4aafc102040b1f079c74fe665
Vadim <Shamray-95@bk.ru>
2022-07-29 16:35:06 +0700

    delete unsorted solutions

--- 1.17.scm
+++
@@ -1,7 +0,0 @@
-(define (double x) (* x 2))
-(define (halve x) (/ x 2))
-(define (mult a b)
-  (cond
-    ((= b 0) 0)
-    ((even? b) (double (mult a (halve b))))
-    (else (+ a (mult a (- b 1))))))

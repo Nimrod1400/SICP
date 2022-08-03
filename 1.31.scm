f86dd6023ef269aad71e270a4e10222e1490a46f
Vadim <Shamray-95@bk.ru>
2022-08-03 16:14:19 +0700

    1.31 updated

--- 1.31.scm
+++ 1.31.scm
@@ -1,8 +1,10 @@
 (define (inc a) (+ a 1))
 
+(define (plus-two a) (+ a 2))
+
 (define (product-recursive term a next b)
   (if (> a b) 
-    1
+    1.0
     (* (term a) 
        (product-recursive term (next a) next b))))
 
@@ -11,4 +13,19 @@
     (if (> a b)
       result
       (iter (next a) (* result (term a)))))
-  (iter a 1))
+  (iter a 1.0))
+
+
+(define (pi n) 
+  (define (new-square a)
+    (cond 
+      ((= (remainder n 4) 0) 
+       (if (= a n) 
+	 a
+	 (square a)))
+      (else 
+	(if (= a n)
+	  a
+	  (square a)))))
+  (* 2.0 (/ (product-iterative new-square 2.0 plus-two n)
+	    (product-iterative new-square 3.0 plus-two (- n 1)))))

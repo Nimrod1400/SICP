3069ec992a3ab4fe7d43f8a96d28a7c9b501c6fb
Vadim <Shamray-95@bk.ru>
2022-08-04 08:59:12 +0700

    1.33 (a) done

---
+++ 1.33.scm
@@ -0,0 +1,41 @@
+(define (expmod base ex m)
+  (cond
+    ((= ex 0) 1)
+    ((even? ex)
+     (remainder (square (expmod base (/ ex 2) m))
+                m))
+    (else
+      (remainder (* base (expmod base (- ex 1) m))
+                 m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (prime? n)
+  (define (iter times)
+    (cond
+      ((even? n) #f)
+      ((= times 0) #t)
+      ((fermat-test n) (iter (- times 1)))
+      (else #f)))
+  (iter 100))
+
+(define (inc a) (+ a 1))
+
+(define (filtered-accumulate combiner null-value term a next b filtered?)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (if (filtered? a)
+	(iter (next a) 
+	      (combiner (term a) result))
+	(iter (next a) result))))
+  (iter a null-value))
+
+(define (filtered-sum term a next b filtered?)
+  (filtered-accumulate + 0 term a next b filtered?))
+
+(define (squares-of-primes a b)
+  (filtered-sum square a inc b prime?))

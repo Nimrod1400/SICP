68ab1543584e26ae5cdf4dc737a7be62fcfbb0cd
schemer <shamray-95@bk.ru>
2023-02-03 17:50:28 +0700

    2.23 fixed

--- 2.2/2.23.scm
+++ 2.2/2.23.scm
@@ -1,7 +1,3 @@
 (define (foreach f l)
   (cond ((null? l) '())
-	(else (f (car l)) (foreach f (cdr l)))))
-
-(define (display-list l)
-  (foreach (lambda (x) (newline) (display x)) 
-	   l))
+	else (f (car l)) (foreach f (cdr l)))))
